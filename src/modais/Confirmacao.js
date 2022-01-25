5f9072fe041afdaf731de80c2f2d7a01a79d9191
macardoso510 <macardoso510@gmail.com>
2022-01-25 13:11:57 -0300

    correçoes de estilo

--- src/modais/Confirmacao.js
+++ src/modais/Confirmacao.js
@@ -30,9 +30,19 @@ const styles = StyleSheet.create({
         margin: 15
     },
     modal: {
-        backgroundColor: '#fff',
+       
+        backgroundColor: '#FFFEF1',
         width: '100%',
-        height: 350,
+        height: 300,
+        // alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
     },
     text: {
         textAlign: 'center',
